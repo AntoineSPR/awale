c784f66a2bc5b30afea38b4b5120c1bfc5cb1d8f
AntoineSPR <antoine.simper@gmail.com>
2024-04-08 18:21:29 +0200

    Saute le trou d'origine si double-tour

--- script.js
+++ script.js
@@ -9,14 +9,20 @@ for (let i = 11; i >= 0; i--) {
     trou[i] = document.getElementById(i.toString());  // Lie les variables à chaque div du document
     trou[i].innerText = n[i];                           // Attribue à chaque trou son nombre de graines
 
+    /**
+     * Au passage de la souris
+     */
+
     trou[i].addEventListener("mouseover", () => {       // Fonction hover pour afficher les possibilités
         Object.values(trou).forEach(element => {        // Réinitialise les bordures avant tout changement
             element.style.borderColor = "initial";
         });
 
         let cible = (i + n[i]) % 12;
+
         if (i < 6) {                         // Ne fonctionne que sur la barre du bas
             if ((i + n[i]) >= 6) {                     // Mouvement valide seulement s'il dépasse la barre du bas
+                if (n[i] >= 12) { cible++; }          // Saute le trou initial en cas de double tour
                 trou[i].style.borderColor = "blue";
                 trou[cible].style.borderColor = "yellow";
             } else if (n[i] > 0) {                      // Mouvement invalide si le trou est rempli mais pas assez
@@ -25,17 +31,25 @@ for (let i = 11; i >= 0; i--) {
         }
     });
 
+    /**
+     * Après le passage de la souris
+     */
+
     trou[i].addEventListener("mouseout", () => {    // Réinitialisation quand hover prend fin
         Object.values(trou).forEach(element => {
             element.style.borderColor = "initial";
         });
     });
 
+    /**
+     * Au clic de la souris
+     */
+
     trou[i].onclick = () => {                       // Fonction lancée au clic
         let cible = (i + n[i]) % 12;
+        if (n[i] >= 12) { cible++; }
         let j = i;                          // Variable arbitraire qui prend la place de i pour s'incrémenter jusqu'à 11
         let k = j;                          // Variable arbitraire qui s'incrémente en parallèle de j, sans limite
-        let priseDePointsLancée = false;
         let priseDePoints = () => {             // Fonction d'implémentation du score et vidage des trous pris
             while (cible >= 6 && (n[cible] === 2 || n[cible] === 3)) {    // Vérifie que les conditions sont remplies pour s'éxécuter
                 score += n[cible];
@@ -48,31 +62,28 @@ for (let i = 11; i >= 0; i--) {
         }
         let tourDeJeu = () => {                     // Lancement de la mécanique de jeu si mouvement valide
             if (trou[i].style.borderColor !== "red") { // Utilisation de la couleur pour éviter les conflits
-                console.log(n[i]);
-                if (i!==j){            // Icrémente de 1 chaque montant, sauf le trou initial
+                if (i !== j) {            // Icrémente de 1 chaque montant, sauf le trou initial
                     n[j]++;
                 }
                 trou[j].innerText = n[j];
                 trou[j].style.opacity = "100%";         // Dégrise les trous remplis
                 trou[i].innerText = 0;       // Affiche un trou d'origine vide dès le début
+                trou[i].style.opacity = "70%";
                 if (n[i] < 12) {           // Vérifie qu'il n'y a qu'un seul tour de plateau à effectuer
-                    console.log("cas 1");
                     if (j !== cible) {  // Incrémente tant que la cible n'est pas atteinte
                         j = (j + 1) % 12;           // Prend en compte les valeurs de cible qui repassent par zéro
                     } else if (j === cible) {       // Une fois la cible atteinte, lance la fonction de prise de points
                         priseDePoints();
+                        n[i] = 0;
                         return;
                     };
                 } else {                   // En cas de second tour de plateau, permet d'éxecuter la progression jusqu'au bout
-                    console.log("cas 2");
                     if (j !== cible || (j === cible && k < 12)) {  // Incrémente tant que la véritable cible n'est pas atteinte
                         j = (j + 1) % 12;
                         k++;
-                        console.log("tour suivant");
                     } else if (j === cible && k >= 12) {      // La cible peut être atteinte une fois qu'un premier tour a été effectué
                         priseDePoints();
-                        priseDePointsLancée = true;
-                        console.log("pris!");
+                        n[i] = 0;
                         return;
                     };
                 }
@@ -81,13 +92,5 @@ for (let i = 11; i >= 0; i--) {
         }
 
         tourDeJeu();
-
-        if (priseDePointsLancée){
-        n[i] = 0;          // Vide le trou d'origine à la toute fin pour ne pas interférer avec le programme
-        }
-
-        if (n[i] === 0) {                       // Grise les trous vides après clic
-            trou[i].style.opacity = "70%";      // (Pas de rouge = pas de conflit avec la boucle de jeu)
-        }
     }
 }
